26866f1ac125d732c2e9a45d7fdf4826e3e0ff90
sharpc42 <chris.m.sharp@gmail.com>
2024-05-21 10:17:17 -0700

    Basic outline of code file

--- stiff-springs.py
+++ stiff-springs.py
@@ -1,1 +1,21 @@
 # tentative stiff springs project
+
+# function to handle user input either through
+# file or through console line input
+def handle_input():
+    return 0
+
+# likely Crank-Nicolson numerical integration
+# for purposes of stability
+def integrator(x):
+    return 0
+
+# plot displacement output for each time step
+# as computed by integrator
+def plot_output():
+    return 0
+
+# displacement output generates wave form
+# which can be used to generate sound file
+def generate_sound_file():
+    return 0
